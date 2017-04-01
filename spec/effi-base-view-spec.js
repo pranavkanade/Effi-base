5409eeb72b372126ed5060fe6c33b2ae9a6815fb
farziCoder <k.pranav.apk@gmail.com>
2017-04-01 11:07:45 +0530

    plugin is shooting errors to json file

---
+++ spec/effi-base-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EffiBaseView from '../lib/effi-base-view';
+
+describe('EffiBaseView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
